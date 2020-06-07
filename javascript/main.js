49c899abd4e26cd4cac56594b857ff2c594bd68f
nathan-n-poon <nathannpoon@gmail.com>
2020-06-07 01:04:52 -0600

    bruhhhhhhhhhh

--- javascript/main.js
+++ javascript/main.js
@@ -20,3 +20,33 @@ function topFunction() {
       }
   );
 }
+
+function vh(v) {
+  var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+  return (v * h) / 100;
+}
+
+function vw(v) {
+  var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+  return (v * w) / 100;
+}
+
+const min = vw(10);
+const max =  vw(20);
+const showMeTheMorty = document.getElementById('showMeTheMorty');
+while (true)
+{
+  setInterval(vary(showMeTheMorty, min, max), 10000);
+}
+
+function vary(showMeTheMorty, min, max) {
+  console.log(showMeTheMorty);
+  var delta = 5 * Math.random();
+  var direction = Math.random > 0.5 ? -1 : 1;
+  var size = showMeTheMorty.style.size;
+  if (direction*delta + size > max || direction*delta + size < min)
+  {
+    direction *= -1;
+  }
+  showMeTheMorty.style.size += direction * delta;
+}
