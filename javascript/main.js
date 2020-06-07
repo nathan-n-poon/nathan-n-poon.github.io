53d990e0fa398cadc8ab6efa1af51e7c4f1b4559
nathan-n-poon <nathannpoon@gmail.com>
2020-06-07 01:36:23 -0600

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -31,22 +31,25 @@ function vw(v) {
   return (v * w) / 100;
 }
 
-const min = vw(10);
-const max =  vw(20);
-const showMeTheMorty = document.getElementById('showMeTheMorty');
-while (true)
-{
-  setInterval(vary(showMeTheMorty, min, max), 10000);
-}
-
 function vary(showMeTheMorty, min, max) {
-  console.log(showMeTheMorty);
+  console.log(showMeTheMorty.style.size);
   var delta = 5 * Math.random();
   var direction = Math.random > 0.5 ? -1 : 1;
   var size = showMeTheMorty.style.size;
-  if (direction*delta + size > max || direction*delta + size < min)
+  if (vw(direction*delta + size) > max || vw(direction*delta + size) < min)
   {
     direction *= -1;
   }
-  showMeTheMorty.style.size += direction * delta;
+  console.log(direction*delta);
+  showMeTheMorty.style.size = vw(size + direction * delta);
+  console.log("end");
 }
+
+const min = vw(10);
+const max =  vw(20);
+const showMeTheMorty = document.getElementById('showMeTheMorty');
+// while (true)
+// {
+  setInterval(function () {vary(showMeTheMorty, min, max)}, 5);
+  console.log("what");
+// }
