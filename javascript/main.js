5554c4b3857f1a7cdc7ca34154c18cfdf7e18a81
nathan-n-poon <nathannpoon@gmail.com>
2020-07-01 00:16:15 -0600

    js

--- javascript/main.js
+++ javascript/main.js
@@ -20,3 +20,22 @@ function topFunction() {
       }
   );
 }
+
+//https://css-tricks.com/flex-grow-is-weird/
+//Author: Manuel Matuzovic
+//css-tricks.com
+
+getDivHeight();
+
+function getDivHeight(divID) {
+  var size = 0
+  for (var node of document.querySelectorAll(divID)) {
+    size += node.getBoundingClientRect().height+"px";
+  }
+  console.log(`size is ${size} px`)
+  document.getElementById("#"+divID).setAttribute("style",`height: max( ${size}px, 100vh)`);
+}
+
+window.onresize = function(event) {
+  getDivHeight('div2');
+};
